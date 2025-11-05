622a48394da32f58f0d3d81642646146152de227
realtimml <timmyphan066@gmail.com>
2025-11-04 23:18:13 -0800

    Create InfoBoxWithHeading component

--- frontend/src/components/InfoBoxWithHeading.tsx
+++ frontend/src/components/InfoBoxWithHeading.tsx
@@ -1,7 +1,20 @@
-const InfoBoxWithHeading = () => {
-  return (
-    <div>InfoBoxWithHeading</div>
-  )
+interface InfoBoxWithHeadingProps {
+  heading: string;
+  body: string;
 }
 
-export default InfoBoxWithHeading
+
+const InfoBoxWithHeading = ({heading, body}: InfoBoxWithHeadingProps) => {
+  return (
+    <div className="card w-80 border-solid border-black border-3">
+      <div className="card-body">
+        <h2 className="card-title text-xl">{heading}</h2>
+        <p className="text-sm">
+          {body}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBoxWithHeading;
