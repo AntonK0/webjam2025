9be27046534c7ee0aab3b22d7ecdda9387d76c43
realtimml <timmyphan066@gmail.com>
2025-11-04 23:59:04 -0800

    Create TextBox component

---
+++ frontend/src/components/TextBox.tsx
@@ -0,0 +1,20 @@
+interface TextBoxProps {
+  placeholder_text: string;
+  text: string;
+  setText: (value: string) => void;
+}
+
+const TextBox = ({ placeholder_text, text, setText }: TextBoxProps) => {
+  return (
+    <textarea
+      value={text}
+      onChange={(e) => {
+        setText(e.target.value);
+      }}
+      className="textarea textarea-ghost border-solid border-black border-2"
+      placeholder={placeholder_text}
+    ></textarea>
+  );
+};
+
+export default TextBox;
