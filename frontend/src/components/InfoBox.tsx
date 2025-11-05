f982679041c38161d75db1b66d895b7879ba57d5
realtimml <timmyphan066@gmail.com>
2025-11-04 23:28:17 -0800

    Create InfoBox component

---
+++ frontend/src/components/InfoBox.tsx
@@ -0,0 +1,18 @@
+interface InfoBoxProps {
+  body: string;
+}
+
+
+const InfoBox = ({body}: InfoBoxProps) => {
+  return (
+    <div className="card w-80 border-solid border-black border-3">
+      <div className="card-body">
+        <p className="text-sm">
+          {body}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBox;
