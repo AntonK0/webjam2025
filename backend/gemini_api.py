1339bc724bda73e8c6571c35149913cd2617096d
Anton Ko <antonk1@uci.edu>
2025-11-04 16:47:27 -0800

    create gemini_api.py and update requirements.txt

---
+++ backend/gemini_api.py
@@ -0,0 +1,15 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)
