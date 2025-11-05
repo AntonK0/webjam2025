8eb9ddb1dbf6f2abaee98b28fd0d5996e8d8089a
kennethyandell <dontclicker@gmail.com>
2025-11-04 23:34:17 -0800

    My stuff is messed up

--- backend/gemini_api.py
+++ backend/gemini_api.py
@@ -6,7 +6,7 @@ load_dotenv()
 api_key= os.getenv("GEMINI_API_KEY")
 GEMINI_API_KEY = api_key
 
-
+# testing for blah blah 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client(api_key=GEMINI_API_KEY)
 
