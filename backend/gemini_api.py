c870a9a1c975373108cdd70415de3f33693e1a18
Anton Ko <antonk1@uci.edu>
2025-11-04 23:08:50 -0800

    make gemini api key work

--- backend/gemini_api.py
+++ backend/gemini_api.py
@@ -3,11 +3,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
+api_key= os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = api_key
 
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client()
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents="Explain how AI works in a few words"
