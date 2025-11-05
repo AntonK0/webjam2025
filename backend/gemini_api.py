219ab115dda47275081d7732cb69459f3778fc8e
Anton Ko <antonk1@uci.edu>
2025-11-04 23:57:20 -0800

    restruct file into smaller functions

--- backend/gemini_api.py
+++ backend/gemini_api.py
@@ -10,9 +10,17 @@ GEMINI_API_KEY = api_key
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client(api_key=GEMINI_API_KEY)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
-# comment for testing purposes
-# comment for creating new backend branch
+def create_prompt():
+    pass
+
+def get_gemini_response(prompt: str) -> str:
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=prompt
+    )
+    return response.text
+
+def main():
+    print(get_gemini_response("Explain the theory of relativity in simple terms."))
+
+if __name__ == "__main__":
+    main()
