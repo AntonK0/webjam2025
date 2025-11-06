c6b1aa22af473d04631028f55e3c7f74fc14efd3
realtimml <timmyphan066@gmail.com>
2025-11-05 22:13:47 -0800

    Implement second page of Assessment

--- frontend/src/components/AssessmentPage.tsx
+++ frontend/src/components/AssessmentPage.tsx
@@ -5,11 +5,13 @@ import {
   RiDropFill,
   RiToothLine,
   RiHandSanitizerLine,
+  RiReservedLine,
 } from "@remixicon/react";
 
 const AssessmentPage = () => {
   const [page, setPage] = useState(0);
   const [productType, setProductType] = useState("");
+  const [hairType, setHairType] = useState("");
 
   if (page == 0) {
     return (
@@ -49,8 +51,56 @@ const AssessmentPage = () => {
         </div>
       </>
     );
+  } else if (page == 1) {
+    return (
+      <>
+        <div>
+          <h1 className="text-3xl text-center font-semibold mt-40">
+            What is your hair type?
+          </h1>
+          <div className="flex justify-center flex-wrap mt-10">
+            <ItemType
+              item="Straight"
+              icon={<RiReservedLine />}
+              onClick={() => {
+                setHairType("Straight");
+                setPage(2);
+              }}
+            />
+            <ItemType
+              item="Curly"
+              icon={<RiReservedLine />}
+              onClick={() => {
+                setHairType("Curly");
+                setPage(2);
+              }}
+              clickable={false}
+            />
+            <ItemType
+              item="Wavy"
+              icon={<RiReservedLine />}
+              onClick={() => {
+                setHairType("Wavy");
+                setPage(2);
+              }}
+              clickable={false}
+            />
+            <ItemType
+              item="Coily"
+              icon={<RiReservedLine />}
+              onClick={() => {
+                setHairType("Coily");
+                setPage(2);
+              }}
+              clickable={false}
+            />
+          </div>
+        </div>
+      </>
+    );
   } else {
-    return <></>;
+    return <>
+      </>;
   }
 };
 
