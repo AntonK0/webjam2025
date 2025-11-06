458918130df06c4d87e7967484cbe15cac74d5c8
DarrenZhao01 <zhaod579@gmail.com>
2025-11-05 23:35:02 -0800

    Implement React Router for home and assessment-page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+// import uploadImageMain from './uploadImageMain.jsx'
+import AssessmentPage from './components/AssessmentPage.js';
+
+const router = createBrowserRouter([
+  {path: "/", element: <App />},
+  {path: "/assessment", element: <AssessmentPage />},
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
 
