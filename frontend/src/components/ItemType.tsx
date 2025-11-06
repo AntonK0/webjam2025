bcafccc2311c64b4d6901a1ec18958d5c99457d4
realtimml <timmyphan066@gmail.com>
2025-11-05 21:17:40 -0800

    Add clickable prop to ItemType (To Be Implemented)

--- frontend/src/components/ItemType.tsx
+++ frontend/src/components/ItemType.tsx
@@ -4,11 +4,12 @@ interface ItemTypeProp {
     item: string;
     icon: ReactNode;
     onClick: () => void;
+    clickable?: boolean;
 }
 
-const ItemType = ({ item, icon, onClick }: ItemTypeProp) => {
+const ItemType = ({ item, icon, onClick, clickable = true }: ItemTypeProp) => {
   return (
-    <button onClick={onClick} className="btn btn-outline border-2 w-[301px] p-3 h-auto ">
+    <button onClick={onClick} className="btn btn-outline border-2 w-[301px] p-3 h-auto mt-4">
       <div className="flex">
         {icon}
         <p className="text-xl ml-4">{item}</p>
