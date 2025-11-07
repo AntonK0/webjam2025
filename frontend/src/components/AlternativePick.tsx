522faf31d2f4bcc0827dc1841de1dd1c92d6be48
realtimml <timmyphan066@gmail.com>
2025-11-06 20:49:55 -0800

    Create AlternativePick Component

---
+++ frontend/src/components/AlternativePick.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+interface AlternativePickProps {
+  productName: string;
+  productDescription: string;
+  onExpand?: () => void;
+}
+
+const AlternativePick: React.FC<AlternativePickProps> = ({ 
+  productName, 
+  productDescription,
+  onExpand 
+}) => {
+  const expandIcon = "http://localhost:3845/assets/dbbb263b2e17194dc87b625234c2a5e6aaa8361e.svg";
+
+  return (
+    <div 
+      className="relative w-full border-black border-[3px] border-solid rounded-[16px] bg-white p-4 min-h-[100px]"
+      data-name="Product Card Mobile"
+    >
+      {/* Expand button - top right corner */}
+      
+      {onExpand && ( 
+        <button
+          onClick={onExpand}
+          className="absolute top-3 right-3 w-4 h-4 cursor-pointer transition-opacity hover:opacity-70 z-10"
+          aria-label="Expand"
+        >
+          <img 
+            alt="Expand" 
+            className="block w-full h-full" 
+            src={expandIcon} 
+          />
+        </button>
+      )}
+
+      {/* Content Container */}
+      <div className="pr-6 flex flex-col gap-1">
+        {/* Product Name */}
+        <h3 className="font-bold text-xl leading-tight text-black">
+          {productName}
+        </h3>
+
+        {/* Product Description */}
+        <p className="font-normal text-xs sm:text-base leading-normal text-black">
+          {productDescription}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AlternativePick;
+
