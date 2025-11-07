956bdd5a59ef40687cf213d22ee20d5ce0f39472
realtimml <timmyphan066@gmail.com>
2025-11-06 20:50:19 -0800

    Create Results Page

--- frontend/src/pages/Results.tsx
+++ frontend/src/pages/Results.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import TopPick from "../components/TopPick";
-import ProductCardMobile from "../components/ProductCardMobile";
+import ProductCardMobile from "../components/SecondPick";
+import { RiSparklingFill, RiSparklingLine } from "@remixicon/react";
+import AlternativePick from "../components/AlternativePick";
 
 const Results = () => {
   const response = useLocation().state;
-
   console.log(response);
 
   if (!response) {
@@ -18,11 +19,17 @@ const Results = () => {
 
   return (
     <>
-    <div className="wrapper min-h-screen flex items-center justify-center">
+    <div className="wrapper pt-24 pl-12 pr-12 flex justify-center flex-wrap">
+      <h1 className="text-3xl font-semibold mb-10">Here's what we recommend.</h1>
+      <div className="w-full flex mb-2"><RiSparklingFill/><h2 className="ml-1 font-semibold"> Our Top Pick</h2></div>
       <TopPick
         productName={response.topPickTitle}
         productDescription={response.topPickType}
       />
+      <div className="w-full flex mt-9 mb-2"><RiSparklingLine/><h2 className="ml-1 font-semibold">Second Pick</h2></div>
+      <ProductCardMobile productName={response.secondPickTitle} productDescription={response.alternativePickType} />
+      <div className="w-full flex mt-9 mb-2"><h2 className="ml-1 font-semibold">Alternative Pick</h2></div>
+      <AlternativePick productName={response.alternativePickTitle} productDescription={response.alternativePickType}/>
     </div>
     </>
   );
