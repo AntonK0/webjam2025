7cd05d993bd2719cca8b658c56f6ad3800875e50
realtimml <timmyphan066@gmail.com>
2025-11-06 23:19:30 -0800

    Fix Formatting

--- frontend/src/pages/Results.tsx
+++ frontend/src/pages/Results.tsx
@@ -20,16 +20,32 @@ const Results = () => {
   return (
     <>
     <div className="wrapper pt-24 pl-12 pr-12 flex justify-center flex-wrap">
-      <h1 className="text-3xl font-semibold mb-10">Here's what we recommend.</h1>
-      <div className="w-full flex mb-2"><RiSparklingFill/><h2 className="ml-1 font-semibold"> Our Top Pick</h2></div>
-      <TopPick
-        productName={response.topPickTitle}
-        productDescription={response.topPickType}
-      />
-      <div className="w-full flex mt-9 mb-2"><RiSparklingLine/><h2 className="ml-1 font-semibold">Second Pick</h2></div>
-      <ProductCardMobile productName={response.secondPickTitle} productDescription={response.alternativePickType} />
-      <div className="w-full flex mt-9 mb-2"><h2 className="ml-1 font-semibold">Alternative Pick</h2></div>
-      <AlternativePick productName={response.alternativePickTitle} productDescription={response.alternativePickType}/>
+        <h1 className="text-3xl font-semibold mb-10">
+          Here's what we recommend.
+        </h1>
+        <div className="w-full flex mb-2">
+          <RiSparklingFill />
+          <h2 className="ml-1 font-semibold"> Our Top Pick</h2>
+        </div>
+        <TopPick
+          productName={response.topPickTitle}
+          productDescription={response.topPickType}
+        />
+        <div className="w-full flex mt-9 mb-2">
+          <RiSparklingLine />
+          <h2 className="ml-1 font-semibold">Second Pick</h2>
+        </div>
+        <ProductCardMobile
+          productName={response.secondPickTitle}
+          productDescription={response.alternativePickType}
+        />
+        <div className="w-full flex mt-9 mb-2">
+          <h2 className="ml-1 font-semibold">Alternative Pick</h2>
+        </div>
+        <AlternativePick
+          productName={response.alternativePickTitle}
+          productDescription={response.alternativePickType}
+        />
     </div>
     </>
   );
